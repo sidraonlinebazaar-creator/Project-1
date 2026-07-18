293a36d5b554d0d339541ee42ed1f11c5571213b
sidraonlinebazaar-creator <sidra.onlinebazaar@gmail.com>
2026-07-18 19:57:21 +0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+Print ("Hello")
+name = input("What is your name?")
+message = input("How can i help you")
+if message == "What is my name":
+print ("Your name is " name)
+else 
+print ("I dont understand that yet")
